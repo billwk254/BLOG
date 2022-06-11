b696667a533f5ea4686c2cddf2411adc59847b9f
billwk254 <billkihungu51@gmail.com>
2022-06-11 22:50:02 +0300

    Added in a tagging system that suggests other posts based on the tags

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -4,6 +4,8 @@ from django.core.paginator import Paginator, EmptyPage,PageNotAnInteger
 from django.views.generic import ListView
 from .forms import EmailPostForm,CommentForm
 from django.core.mail import send_mail
+from taggit.models import Tag
+from django.db.models import Count 
 # Create your views here.
 
 def post_share(request, post_id):
@@ -21,6 +23,25 @@ def post_share(request, post_id):
     else:
         form = EmailPostForm()
     return render(request, 'blog/post/share.html',{'post':post,'form':form,'sent':sent})
+
+def post_list(request, tag_slug=None):
+    object_list = Post.published.all()
+    tag = None
+
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
+
+    paginator = Paginator(object_list, 3)
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    return render(request, 'blog/post/list.html', {'page':page,
+    'posts':posts,'tag':tag})
 
 
 class PostListView(ListView):
@@ -45,5 +66,9 @@ def post_detail(request,year,month,day,post):
     
     else:
         comment_form = CommentForm()
+    
+    post_tags_ids = post.tags.values_list('id',flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
 
-    return render(request, 'blog/post/detail.html',{'post':post,'comments': comments,'new_comment': new_comment,'comment_form':comment_form})
+    return render(request, 'blog/post/detail.html',{'post':post,'comments': comments,'new_comment': new_comment,'comment_form':comment_form, 'similar_posts':similar_posts})
