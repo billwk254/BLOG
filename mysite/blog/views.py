3b8737d40891d62fc875096d9065db91294af8fa
billwk254 <billkihungu51@gmail.com>
2022-06-23 10:53:41 +0300

    text similarity measurement and index searching based on trigrams

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -6,7 +6,8 @@ from .forms import EmailPostForm,CommentForm,SearchForm
 from django.core.mail import send_mail
 from taggit.models import Tag
 from django.db.models import Count 
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVector,SearchQuery,SearchRank
+from django.contrib.postgres.search import TrigramSimilarity
 # Create your views here.
 
 def post_share(request, post_id):
@@ -82,6 +83,8 @@ def post_search(request):
         form = SearchForm(request.GET) 
         if form.is_valid():
             query = form.cleaned_data['query']
-            results = Post.published.annotate(search=SearchVector('title', 'body'),).filter(search=query)
+            search_vector = SearchVector('title', weight='A') + SearchVector('body', weight='B')
+            search_query = SearchQuery(query)
+            results = Post.published.annotate(similarity = TrigramSimilarity('title', query),).filter(similarity__gt=0.1).order_by('-similarity')
     return render(request, 'blog/post/search.html',{'form':form,'query':query,'results':results})
     
