6be699090d08d7542fb3514e258ac9162a386fff
billwk254 <billkihungu51@gmail.com>
2022-06-30 13:57:32 +0300

    Allowed hosts added

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = '^)qu+a(+68ln1d$u_(_8m6qd3_*jd4o#@g4xj&#2%@qz7=!544'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['wamboo-blog.herokuapp.com']
 SITE_ID = 1
 
 
